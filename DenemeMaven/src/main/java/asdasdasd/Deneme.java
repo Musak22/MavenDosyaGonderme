9e4e4fd33ebecdace3fa073eab508bc58152998b
Musak22 <beratucar@outlook.com>
2023-05-02 21:13:52 +0300

    Eklendi Gibi

---
+++ DenemeMaven/src/main/java/asdasdasd/Deneme.java
@@ -0,0 +1,15 @@
+package asdasdasd;
+import org.apache.commons.lang3.StringUtils;
+
+public class Deneme {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(StringUtils.deleteWhitespace("   ab   c   "));
+	}
+
+}
